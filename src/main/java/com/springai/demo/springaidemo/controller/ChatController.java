428e4155e3f032690fb08ce6d98310604e40e893
SWAPNIL <swapnil14392@gmail.com>
2024-01-29 22:52:26 +0530

    spring ai implementation

---
+++ src/main/java/com/springai/demo/springaidemo/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.springai.demo.springaidemo.controller;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    @Autowired
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+
+    }
+    @GetMapping("/open-ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+}
